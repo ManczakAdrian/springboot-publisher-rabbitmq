e3ed69e8c5ae79c132cb2c7616d8b2c8c9069b11
Manczak Adrian <manczakadrian@gmail.com>
2020-11-02 00:43:11 +0100

    Initial commit

---
+++ src/main/java/pl/manczak/springbootpublisherrabbitmq/SpringbootPublisherRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package pl.manczak.springbootpublisherrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPublisherRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPublisherRabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/manczak/springbootpublisherrabbitmq/SpringbootPublisherRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manczak.springbootpublisherrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPublisherRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
